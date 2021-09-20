c20445b80268b4aa28e863ffa6d016daa59fac08
giraffeSeren <qiang.serengetti@gmail.com>
2021-09-20 00:14:12 -0400

    add a repso for test

---
+++ manufacturing-result-crud/src/main/java/com/mytech/repository/ManufacturingRepo.java
@@ -0,0 +1,9 @@
+package com.mytech.repository;
+
+/**
+ * @author `<a href="mailto:qiang.wang@1020@gmail.com">qiang</a>`
+ * @date 2021-09-20
+ * @description :
+ */
+public class ManufacturingRepo {
+}
