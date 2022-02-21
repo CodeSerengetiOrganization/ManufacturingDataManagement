50c20bae661821aeeb0fd2d6efdad3d11402a1cd
giraffeSeren <qiang.serengetti@gmail.com>
2022-02-21 15:52:42 -0500

    1. add cover local file method to ManufacturingService;2. not runnable, as there is 'not managed type' error when running, seems from previous branch

---
+++ manufacturing-result-crud/src/test/java/com/mytech/ManufacturingResultServiceTests.java
@@ -0,0 +1,9 @@
+package com.mytech;
+
+/**
+ * @author `<a href="mailto:qiang.wang@1020@gmail.com">qiang</a>`
+ * @date 2022-02-21
+ * @description :
+ */
+public class ManufacturingResultServiceTests {
+}
