16622c3deee3dc26bf13d638c7b2f753ec69993b
ProgrammerBSeren <programmerb.serengeti@gmail.com>
2022-04-27 18:50:10 -0400

    service layer could extract test result(PASS/FAIL),robotStation,ERP,manufacturingTime,shiftNo and serialNo from file name;

--- local-result-convert/src/main/java/com/mytech/service/LocalResultConvertService.java
+++ local-result-convert/src/main/java/com/mytech/service/LocalResultConvertService.java
@@ -2,6 +2,7 @@ package com.mytech.service;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -11,6 +12,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.JulianFields;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,24 +50,61 @@ public class LocalResultConvertService {
     private void parseLocalTestFile(File file,int arrSizeIfPass,int arrSizeIfFail,String failSymbolString) throws FileNotFoundException {
         String[] splitFileName = new String[0];
         String testResult =null;
+        String robotStation =null;
+        String erpNo =null;
+        String julianDay =null;
+        String shiftNo =null;
+        String serialNo =null;
+        String timeString =null;
+        LocalDate localDateYearDay = null;
+        LocalTime localTime=null;
+        LocalDateTime manufacturingTime=null;
+//        String chronoDayString=null;
+        int normalStartIndex =0;
         if (!file.exists()) throw new FileNotFoundException("File ["+file.getName()+"] is not found");
         if (file !=null){
-            splitFileName=file.getName().split("_");
+            String fileNameWithoutExtension = FilenameUtils.removeExtension(file.getName());
+            splitFileName=fileNameWithoutExtension.split("_");
             if(splitFileName.length==arrSizeIfFail && splitFileName[0].equals(failSymbolString)){//this is a FAILED product
                 testResult="FAIL";
+                normalStartIndex=1;
             }else if (splitFileName.length==arrSizeIfPass) {
                 // this product is a PASSED product
                 testResult="PASS";
             }else{
                 //unknown result
                 testResult="UNKNOWN";
-            }
-        }
+            }//if-else
+            robotStation=splitFileName[normalStartIndex+1];
+            erpNo=splitFileName[normalStartIndex+2];
+            julianDay=splitFileName[normalStartIndex+3];
+            shiftNo=splitFileName[normalStartIndex+4];
+            serialNo=splitFileName[normalStartIndex+5];
+            timeString=splitFileName[normalStartIndex+6];
+            localDateYearDay = LocalDate.ofYearDay(Integer.parseInt("2022"), Integer.parseInt(julianDay));
+            DateTimeFormatter kirTimeFormatter=DateTimeFormatter.ofPattern("HH.mm.ss");
+            localTime = LocalTime.parse(timeString, kirTimeFormatter);
+            manufacturingTime= LocalDateTime.of(localDateYearDay,localTime);
+//            chronoDayString = LocalDate.MIN.with(JulianFields.JULIAN_DAY, Long.parseLong(julianDay))
+//                    .format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+//            LocalDate.MIN.with(JulianFields.)
+
+        }//if
         System.out.println("splitFileName array size:"+splitFileName.length);
         for (String s:splitFileName) {
             System.out.println(s);
         }
         System.out.println("testResult:"+testResult);
+        System.out.println("robotStation:"+robotStation);
+        System.out.println("erpNo:"+erpNo);
+        System.out.println("julianDay:"+julianDay);
+        System.out.println("localDateYearDay:"+localDateYearDay);
+        System.out.println("localTime:"+localTime);
+        System.out.println("manufacturingTime:"+manufacturingTime);
+        System.out.println("shiftNo:"+shiftNo);
+        System.out.println("serialNo:"+serialNo);
+        System.out.println("timeString:"+timeString);
+
     }//parseLocalTestFile
 
 }//class
